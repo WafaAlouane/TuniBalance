e2ca7b569461c6ebf8804396fdf08077c8243be1
arijbenmerdes <arij2000bms@gmail.com>
2025-03-11 21:20:58 +0100

    2FA + update2 jenkinsfile

--- client/src/pages/BusinessOwner.jsx
+++ client/src/pages/BusinessOwner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/admin/Header'
 import Sidebar from '../components/admin/Sidebar'
-import Dashboard from '../components/admin/dashboard'
+//import Dashboard from '../components/admin/dashboard'
 import Reports from '../components/admin/Reports'
 import Footer from '../components/admin/Footer'
 function BusinessOwner() {
